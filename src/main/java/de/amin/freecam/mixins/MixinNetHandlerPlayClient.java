5e690d5d8ca51f1678033d1e4858ef274ecea199
Amin <amin.haddou@frg.wwschool.de>
2023-01-06 23:07:29 +0100

    change teleport detection method

--- src/main/java/de/amin/freecam/mixins/MixinNetHandlerPlayClient.java
+++ src/main/java/de/amin/freecam/mixins/MixinNetHandlerPlayClient.java
@@ -2,7 +2,7 @@ package de.amin.freecam.mixins;
 
 import de.amin.freecam.FreecamMod;
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.network.play.server.S05PacketSpawnPosition;
+import net.minecraft.network.play.server.S07PacketRespawn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,9 +16,9 @@ public class MixinNetHandlerPlayClient {
      */
     @Inject(
             at = @At("TAIL"),
-            method = "handleSpawnPosition"
+            method = "handleRespawn"
     )
-    public void onJoin(S05PacketSpawnPosition packetIn, CallbackInfo ci) {
+    public void onJoin(S07PacketRespawn packetIn, CallbackInfo ci) {
         if (FreecamMod.isEnabled) FreecamMod.getInstance().disableFreecam();
     }
 
